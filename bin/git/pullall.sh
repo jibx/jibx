65ca31540a819100722ee7103b00d4ee656fed26
Don Corley <don@donandann.com>
2011-11-14 10:14:03 -0800

    Add pullall script

---
+++ bin/git/pullall.sh
@@ -0,0 +1,17 @@
+# git clone git@github.com:jibx/jibx.git
+git pull
+git submodule update
+
+for dir in jibx-parent \
+../core \
+../web-services/jibxws \
+../../web-services/xbis \
+../../sub-projects \
+../plugins/maven-plugin \
+../../plugins/jibx-eclipse \
+../../schema-library; do
+echo $dir
+cd $dir
+git pull origin master
+done
+
